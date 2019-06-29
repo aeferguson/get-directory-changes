53190cc70a15f7d7f21300f2c71f6675628b9f56
Andrew Ferguson <46054196+aeferguson@users.noreply.github.com>
2019-06-29 06:36:10 -0500

    Update scan-dir.ps1
    changed "$dir" to new name "$Path"

--- scan-dir.ps1
+++ scan-dir.ps1
@@ -5,5 +5,5 @@ $searchDate = [datetime]::Today.AddDays(-1).AddHours(07)
 #make this into a parameter too, later
 
 
-Get-ChildItem $dir -File -Recurse | 
+Get-ChildItem $Path -File -Recurse | 
     Where-Object { $_.LastWriteTime -gt ($searchDate)} | select-Object FullName, LastWriteTime -First 100 | Sort-Object -Property LastWriteTime -Descending
